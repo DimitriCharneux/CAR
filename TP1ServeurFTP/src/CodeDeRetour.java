de1ac476fd0151bfde27445adcf49d428c8aebc7
Dimitri Charneux <dimitri15.59@hotmail.fr>
2016-02-17 11:23:33 +0100

    commande dossier

--- TP1ServeurFTP/src/CodeDeRetour.java
+++ TP1ServeurFTP/src/CodeDeRetour.java
@@ -1,47 +1,53 @@
 public class CodeDeRetour {
 	public static final int SERVICE_PRET = 220, ATTENTE_MDP = 331,
 			AUTHENTIFICATION_FAIL = 332, AUTHENTIFICATION_OK = 230,
-			BEGIN_TRANSFERT = 125, END_TRANSFERT = 226, DECONNEXION = 221, NONCONNECTE = 532, SERVICE_OK = 200, ERREUR_ARG = 501;
+			BEGIN_TRANSFERT = 125, END_TRANSFERT = 226, DECONNEXION = 221,
+			NONCONNECTE = 532, SERVICE_OK = 200, ERREUR_ARG = 501,
+			ERREUR = 550;
 
-	public static String servicePret(){
+	public static String servicePret() {
 		return SERVICE_PRET + " : service pret.\n";
 	}
-	
-	public static String attenteMdp(){
+
+	public static String attenteMdp() {
 		return ATTENTE_MDP + " : Entrez votre mot de passe.\n";
 	}
-	
-	public static String authentificationFail(){
+
+	public static String authentificationFail() {
 		return AUTHENTIFICATION_FAIL + " : Erreur d'authentification.\n";
 	}
-	
-	public static String authentificationOk(){
+
+	public static String authentificationOk() {
 		return AUTHENTIFICATION_OK + " : Authentification réussi.\n";
 	}
-	
-	public static String beginTransfert(){
+
+	public static String beginTransfert() {
 		return BEGIN_TRANSFERT + " : Le tranfert à commencé.\n";
 	}
-	
-	public static String endTransfert(){
+
+	public static String endTransfert() {
 		return END_TRANSFERT + " : Le transfert est terminé.\n";
 	}
-	
-	public static String deconnexion(){
+
+	public static String deconnexion() {
 		return DECONNEXION + " : Déconnexion.\n";
 	}
-	
-	
-	public static String nonConnecte(){
-		return NONCONNECTE + " : Vous devez-être connecté pour effectuer cette action.\n";
+
+	public static String nonConnecte() {
+		return NONCONNECTE
+				+ " : Vous devez-être connecté pour effectuer cette action.\n";
 	}
-	
-	public static String serviceOk(){
+
+	public static String serviceOk() {
 		return SERVICE_OK + " : Service ok.\n";
 	}
-	
-	public static String erreurArgs(){
+
+	public static String erreurArgs() {
 		return ERREUR_ARG + " : Erreur dans les arguments.\n";
 	}
-	
+
+	public static String erreur() {
+		return ERREUR + " : Erreur.\n";
+	}
+
 }
--- TP1ServeurFTP/src/FtpRequest.java
+++ TP1ServeurFTP/src/FtpRequest.java
@@ -1,22 +1,20 @@
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.net.InetAddress;
-import java.net.ServerSocket;
 import java.net.Socket;
-import java.net.UnknownHostException;
 
 public class FtpRequest extends Thread {
-	private Socket soc,socData;
+	private Socket soc, socData;
 	private int timeOut;
 	private OutputStreamWriter outputwriter, outputWriterData;
 	private BufferedReader inputreader;
 	private boolean estAuthentifer, veutQuitter;
-	private String utilisateurConnecte, tmpUser;
+	private String utilisateurConnecte, tmpUser, repPrinc;
 	private File repertoire;
 
 	public FtpRequest(Socket soc) {
@@ -47,10 +45,9 @@ public class FtpRequest extends Thread {
 	private void processRequest() throws IOException {
 		String[] cmd = inputreader.readLine().split(" ", 2);
 		System.out.println("commande recu :");
-		for(String tmp : cmd)System.out.println("\t"+tmp);
-		
-		
-		
+		for (String tmp : cmd)
+			System.out.println("\t" + tmp);
+
 		switch (cmd[0]) {
 		case "USER":
 			if (cmd.length < 2) {
@@ -90,9 +87,39 @@ public class FtpRequest extends Thread {
 				send(CodeDeRetour.nonConnecte());
 			break;
 		case "PORT":
-			if (estAuthentifer && cmd.length == 2){
+			if (estAuthentifer && cmd.length == 2) {
 				processPort(cmd[1]);
-			}else
+			} else
+				send(CodeDeRetour.nonConnecte());
+			break;
+		case "STOR":
+			if (estAuthentifer && cmd.length == 2) {
+				processSTOR(cmd[1]);
+			} else
+				send(CodeDeRetour.nonConnecte());
+			break;
+		case "PWD":
+			if (estAuthentifer) {
+				processPWD();
+			} else
+				send(CodeDeRetour.nonConnecte());
+			break;
+		case "CWD":
+			if (estAuthentifer && cmd.length == 2) {
+				processCWD(cmd[1]);
+			} else
+				send(CodeDeRetour.nonConnecte());
+			break;
+		case "MKD":
+			if (estAuthentifer && cmd.length == 2) {
+				processMKD(cmd[1]);
+			} else
+				send(CodeDeRetour.nonConnecte());
+			break;
+		case "CDUP":
+			if (estAuthentifer) {
+				processCDUP();
+			} else
 				send(CodeDeRetour.nonConnecte());
 			break;
 		case "QUIT":
@@ -106,27 +133,30 @@ public class FtpRequest extends Thread {
 
 	private void processPort(String cmd) {
 		String[] tmpPort = cmd.split(",");
-		String tmp="";
-		int port = Integer.parseInt(tmpPort[4])*256 + Integer.parseInt(tmpPort[5]);
-		for(int i=0; i<4; i++)tmp+=tmpPort[i] + ".";
-		tmp = tmp.substring(0, tmp.length()-1);
-		System.out.println("addr : \'"+tmp+"\', port : \'"+port+"\'");
+		String tmp = "";
+		int port = Integer.parseInt(tmpPort[4]) * 256
+				+ Integer.parseInt(tmpPort[5]);
+		for (int i = 0; i < 4; i++)
+			tmp += tmpPort[i] + ".";
+		tmp = tmp.substring(0, tmp.length() - 1);
+		System.out.println("addr : \'" + tmp + "\', port : \'" + port + "\'");
 		try {
 			socData = new Socket(tmp, port);
 			send(CodeDeRetour.serviceOk());
 		} catch (Exception e) {
 			send(CodeDeRetour.erreurArgs());
 		}
-		
+
 	}
 
 	private void processRETR(String pathname) {
 		try {
-			InputStream flux=new FileInputStream(repertoire.getPath() + "/" + pathname); 
-			InputStreamReader lecture=new InputStreamReader(flux);
-			BufferedReader buff=new BufferedReader(lecture);
+			InputStream flux = new FileInputStream(repertoire.getPath() + "/"
+					+ pathname);
+			InputStreamReader lecture = new InputStreamReader(flux);
+			BufferedReader buff = new BufferedReader(lecture);
 			String tmp = "", line;
-			while((line = buff.readLine())!=null){
+			while ((line = buff.readLine()) != null) {
 				tmp += line + "\n";
 			}
 			buff.close();
@@ -137,6 +167,58 @@ public class FtpRequest extends Thread {
 
 	}
 
+	private void processSTOR(String fileName) {
+		File fichier = new File(repertoire.getPath() +"/" +fileName);
+		String data = receiveData();
+		if(data == null)return ;
+		try {
+			fichier.createNewFile();
+			FileOutputStream output = new FileOutputStream(fichier);
+			output.write(data.getBytes());
+			output.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private void processPWD() {
+		System.out.println("path : " + repertoire.getPath());
+		send(repertoire.getPath() + "\n");
+	}
+	
+
+	private void processCWD(String nameRep) {
+		File[] tmp = repertoire.listFiles();
+		for (File rep : tmp) {
+			if (rep.getName().equals(nameRep) && rep.isDirectory()) {
+				repertoire = rep;
+				send(CodeDeRetour.serviceOk());
+				return;
+			}
+		}
+		send(CodeDeRetour.erreur());
+	}
+	
+	private void processMKD(String string) {
+		repertoire = new File(repertoire.getPath() + "/" + string);
+		repertoire.mkdir();
+		repertoire = repertoire.getParentFile();
+		send(CodeDeRetour.serviceOk());
+	}
+
+	private void processCDUP() {
+		System.out.println(repPrinc);
+		System.out.println(repertoire.getPath());
+		
+		if(!repertoire.getPath().equals(repPrinc)){
+			repertoire = repertoire.getParentFile();
+			send(CodeDeRetour.serviceOk());
+		} else {
+			send(CodeDeRetour.erreur());
+		}
+	}
+	
+
 	private void ouvreRepertoire() {
 		repertoire = new File("repPrincipale");
 		repertoire.mkdir();
@@ -144,11 +226,13 @@ public class FtpRequest extends Thread {
 		for (File rep : tmp) {
 			if (rep.getName().equals(utilisateurConnecte) && rep.isDirectory()) {
 				repertoire = rep;
+				repPrinc = repertoire.getPath();
 				return;
 			}
 		}
 		repertoire = new File("repPrincipale/" + utilisateurConnecte);
 		repertoire.mkdir();
+		repPrinc = repertoire.getPath();
 	}
 
 	private void processLIST() {
@@ -175,7 +259,7 @@ public class FtpRequest extends Thread {
 			e.printStackTrace();
 		}
 	}
-	
+
 	private void sendData(String msg) {
 		send(CodeDeRetour.beginTransfert());
 		try {
@@ -187,6 +271,24 @@ public class FtpRequest extends Thread {
 			e.printStackTrace();
 		}
 		send(CodeDeRetour.endTransfert());
+	}
+
+	private String receiveData() {
+		send(CodeDeRetour.beginTransfert());
+		try {
+			BufferedReader br = new BufferedReader(new InputStreamReader(
+					socData.getInputStream()));
+			String res = "", tmp;
+			while((tmp = br.readLine())!=null){
+				res += tmp + "\n";
+			}
+			send(CodeDeRetour.endTransfert());
+			return res;
+		} catch (IOException e) {
+			e.printStackTrace();
+			send(CodeDeRetour.endTransfert());
+			return null;
+		}
 	}
 
 }
