e3f804e9dcd932a2ff940343095a752145d23d2a
Dimitri Charneux <dimitri15.59@hotmail.fr>
2016-04-26 11:05:23 +0200

    tp4

---
+++ TP4JEE/src/main/java/BibliothequeItf.java
@@ -0,0 +1,10 @@
+import java.util.List;
+
+import javax.ejb.Local;
+
+
+@Local
+public interface BibliothequeItf {
+	public void initBiblio();
+	public List<String> retourneAuteurs();
+}
---
+++ TP4JEE/src/main/java/Livre.java
@@ -0,0 +1,47 @@
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Livre {
+	public static int cptId = 0;
+	
+	@Id
+	private int id;
+	private String auteur, titre, annee;
+	
+	public Livre(String auteur, String titre, String annee){
+		this.auteur = auteur;
+		this.titre = titre;
+		this.annee = annee;
+		this.id = cptId;
+		Livre.cptId++;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public String getAuteur() {
+		return auteur;
+	}
+
+	public void setAuteur(String auteur) {
+		this.auteur = auteur;
+	}
+
+	public String getTitre() {
+		return titre;
+	}
+
+	public void setTitre(String titre) {
+		this.titre = titre;
+	}
+
+	public String getAnnee() {
+		return annee;
+	}
+
+	public void setAnnee(String annee) {
+		this.annee = annee;
+	}
+}
